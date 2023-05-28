22ba10847522b2cb3405c7cbde0de7325544ec50
PCS <duythienbui@gmail.com>
2023-05-28 08:19:01 -0600

    c

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -9,11 +9,11 @@ if (!process.env.FIREBASE_PRIVATE_KEY) throw new Error("Missing NEXT_FIREBASE_PR
 if (!process.env.GOOGLE_ID) throw new Error("Missing NEXT_GOOGLE_ID")
 if (!process.env.GOOGLE_SECRET) throw new Error("Missing NEXT_GOOGLE_SECRET")
 
-if (!process.env.FIREBASE_PROJECT_ID) throw new Error("Missing NEXT_FIREBASE_PROJECT_ID")
-if (!process.env.FIREBASE_CLIENT_EMAIL) throw new Error("Missing NEXT_FIREBASE_CLIENT_EMAIL")
-if (!process.env.FIREBASE_PRIVATE_KEY) throw new Error("Missing NEXT_FIREBASE_PRIVATE_KEY")
-if (!process.env.GOOGLE_ID) throw new Error("Missing NEXT_GOOGLE_ID")
-if (!process.env.GOOGLE_SECRET) throw new Error("Missing NEXT_GOOGLE_SECRET")
+if (!process.env.FIREBASE_PROJECT_ID) throw new Error("Missing FIREBASE_PROJECT_ID")
+if (!process.env.FIREBASE_CLIENT_EMAIL) throw new Error("Missing FIREBASE_CLIENT_EMAIL")
+if (!process.env.FIREBASE_PRIVATE_KEY) throw new Error("Missing FIREBASE_PRIVATE_KEY")
+if (!process.env.GOOGLE_ID) throw new Error("Missing GOOGLE_ID")
+if (!process.env.GOOGLE_SECRET) throw new Error("Missing GOOGLE_SECRET")
 
 export const authOptions = {
   // Configure one or more authentication providers
