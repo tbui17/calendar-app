0c7e07f7854dba071e067c3f970963f7c223b16a
PCS <duythienbui@gmail.com>
2023-05-28 07:47:37 -0600

    c

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -3,19 +3,26 @@ import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth"
 import { cert } from "firebase-admin/app";
 
-if (!process.env.NEXT_FIREBASE_PROJECT_ID) throw new Error("Missing NEXT_FIREBASE_PROJECT_ID")
-if (!process.env.NEXT_FIREBASE_CLIENT_EMAIL) throw new Error("Missing NEXT_FIREBASE_CLIENT_EMAIL")
-if (!process.env.NEXT_FIREBASE_PRIVATE_KEY) throw new Error("Missing NEXT_FIREBASE_PRIVATE_KEY")
-if (!process.env.NEXT_GOOGLE_ID) throw new Error("Missing NEXT_GOOGLE_ID")
-if (!process.env.NEXT_GOOGLE_SECRET) throw new Error("Missing NEXT_GOOGLE_SECRET")
+if (!process.env.FIREBASE_PROJECT_ID) throw new Error("Missing NEXT_FIREBASE_PROJECT_ID")
+if (!process.env.FIREBASE_CLIENT_EMAIL) throw new Error("Missing NEXT_FIREBASE_CLIENT_EMAIL")
+if (!process.env.FIREBASE_PRIVATE_KEY) throw new Error("Missing NEXT_FIREBASE_PRIVATE_KEY")
+if (!process.env.GOOGLE_ID) throw new Error("Missing NEXT_GOOGLE_ID")
+if (!process.env.GOOGLE_SECRET) throw new Error("Missing NEXT_GOOGLE_SECRET")
 
 export const authOptions = {
   // Configure one or more authentication providers
-
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      
+    }),
+  }),
   providers: [
     GoogleProvider({
-      clientId: process.env.NEXT_GOOGLE_ID,
-      clientSecret: process.env.NEXT_GOOGLE_SECRET,
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
       
       authorization: {
         
