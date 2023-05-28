9adab08e50db2185f22b639f10b201b5faddb7db
PCS <duythienbui@gmail.com>
2023-05-28 08:16:29 -0600

    c

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -3,6 +3,12 @@ import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth"
 import { cert } from "firebase-admin/app";
 
+if (!process.env.NEXT_FIREBASE_PROJECT_ID) throw new Error("Missing NEXT_FIREBASE_PROJECT_ID")
+if (!process.env.FIREBASE_CLIENT_EMAIL) throw new Error("Missing NEXT_FIREBASE_CLIENT_EMAIL")
+if (!process.env.FIREBASE_PRIVATE_KEY) throw new Error("Missing NEXT_FIREBASE_PRIVATE_KEY")
+if (!process.env.GOOGLE_ID) throw new Error("Missing NEXT_GOOGLE_ID")
+if (!process.env.GOOGLE_SECRET) throw new Error("Missing NEXT_GOOGLE_SECRET")
+
 if (!process.env.FIREBASE_PROJECT_ID) throw new Error("Missing NEXT_FIREBASE_PROJECT_ID")
 if (!process.env.FIREBASE_CLIENT_EMAIL) throw new Error("Missing NEXT_FIREBASE_CLIENT_EMAIL")
 if (!process.env.FIREBASE_PRIVATE_KEY) throw new Error("Missing NEXT_FIREBASE_PRIVATE_KEY")
