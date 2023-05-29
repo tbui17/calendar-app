81604548db2f7957bad2538b571e6104ad8786d5
PCS <duythienbui@gmail.com>
2023-05-29 05:54:26 -0600

    c

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import './globals.css'
 
-import { ClerkProvider } from '@clerk/nextjs'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,10 +15,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
-    </ClerkProvider>
+    
   )
 }
