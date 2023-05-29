8ca9ab4e09c1891fc731826e650ccda152e940a2
PCS <duythienbui@gmail.com>
2023-05-29 05:56:47 -0600

    c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,35 +8,38 @@ import { CalendarApp } from "../components/calendar-components/calendar";
 
 
 export default function Home() {
-	const session = useSession();
-	const { data } = session;
+	// const session = useSession();
+	// const { data } = session;
 
-	if (data) {
-		console.log(data)
-		return (
-			<div>
-				<p>Welcome {data.user?.email}</p>
-				<button
-					className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-					onClick={() => signOut()}
-				>
-					Sign out
-				</button>
-				<CalendarApp />
-			</div>
-		);
-	} else {
-		return (
-			<div>
-				<p>Not signed in.</p>
-				<button
-					title="Sign in"
-					onClick={() => signIn()}
-					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-				>
-					Sign in
-				</button>
-			</div>
-		);
-	}
+
+	return <CalendarApp />
+
+	// if (data) {
+	// 	console.log(data)
+	// 	return (
+	// 		<div>
+	// 			<p>Welcome {data.user?.email}</p>
+	// 			<button
+	// 				className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+	// 				onClick={() => signOut()}
+	// 			>
+	// 				Sign out
+	// 			</button>
+	// 			<CalendarApp />
+	// 		</div>
+	// 	);
+	// } else {
+	// 	return (
+	// 		<div>
+	// 			<p>Not signed in.</p>
+	// 			<button
+	// 				title="Sign in"
+	// 				onClick={() => signIn()}
+	// 				className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+	// 			>
+	// 				Sign in
+	// 			</button>
+	// 		</div>
+	// 	);
+	// }
 }
