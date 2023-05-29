9b0dc096646ef8ebc88d692c6b02b1c698a1d6ae
PCS <duythienbui@gmail.com>
2023-05-29 08:28:32 -0600

    c

--- src/components/calendar-components/calendar2.tsx
+++ src/components/calendar-components/calendar2.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
-import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction"; // needed for dayClick
-
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
-import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import multiMonthPlugin from '@fullcalendar/multimonth'
 
 export default function CalendarAppCopy() {
@@ -12,17 +8,16 @@ export default function CalendarAppCopy() {
         { title: "event 1", date: "2023-04-06" },
         { title: "event 2", date: "2023-04-05" },
     ]
-	const [events, setEvents] = useState(defaultEvents);
 
 	return (
 		<div>
 		
 		<FullCalendar
-			plugins={[dayGridPlugin, interactionPlugin, multiMonthPlugin]}
+			plugins={[multiMonthPlugin]}
 			initialView="multiMonthYear"
 			weekends={true}
 			
-			events={events}
+			events={defaultEvents}
 			
 			headerToolbar={{
 				start: "customButton prev,next today",
