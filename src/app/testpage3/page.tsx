514422fa125682b0f5c82ee584595123fc66000b
PCS <duythienbui@gmail.com>
2023-05-29 08:09:08 -0600

    c

--- src/app/testpage3/page.tsx
+++ src/app/testpage3/page.tsx
@@ -1,40 +1,5 @@
-"use client";
+import CalendarAppCopy from "@/components/calendar-components/calendar2";
 
-import React, { useState } from "react";
-import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction"; // needed for dayClick
-
-import FullCalendar from "@fullcalendar/react"; // must go before plugins
-import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
-import multiMonthPlugin from '@fullcalendar/multimonth'
-
-export const CalendarApp = () => {
-    const defaultEvents = [
-        { title: "event 1", date: "2023-04-06" },
-        { title: "event 2", date: "2023-04-05" },
-    ]
-	const [events, setEvents] = useState(defaultEvents);
-
-	return (
-		<div>
-		
-		<FullCalendar
-			plugins={[dayGridPlugin, interactionPlugin, multiMonthPlugin]}
-			initialView="multiMonthYear"
-			weekends={true}
-			
-			events={events}
-			
-			headerToolbar={{
-				start: "customButton prev,next today",
-				center: "title",
-				end: "dayGridMonth,timeGridWeek,timeGridDay",
-				
-			}}
-			
-			multiMonthMaxColumns={1}
-			
-		/>
-		</div>
-		
-	);
-};
+export default function TestPage3(){
+    return {CalendarAppCopy}
+}
---
+++ src/components/calendar-components/calendar2.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React, { useState } from "react";
+import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction"; // needed for dayClick
+
+import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import multiMonthPlugin from '@fullcalendar/multimonth'
+
+export default async function CalendarAppCopy() {
+    const defaultEvents = [
+        { title: "event 1", date: "2023-04-06" },
+        { title: "event 2", date: "2023-04-05" },
+    ]
+	const [events, setEvents] = useState(defaultEvents);
+
+	return (
+		<div>
+		
+		<FullCalendar
+			plugins={[dayGridPlugin, interactionPlugin, multiMonthPlugin]}
+			initialView="multiMonthYear"
+			weekends={true}
+			
+			events={events}
+			
+			headerToolbar={{
+				start: "customButton prev,next today",
+				center: "title",
+				end: "dayGridMonth,timeGridWeek,timeGridDay",
+				
+			}}
+			
+			multiMonthMaxColumns={1}
+			
+		/>
+		</div>
+		
+	);
+};
