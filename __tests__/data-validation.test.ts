e444e0dbbd09269bf03f8311609dcb0e750c0b30
vqr sn <duythienbui@gmail.com>
2023-06-11 09:28:05 -0600

    c

--- __tests__/data-validation.test.ts
+++ __tests__/data-validation.test.ts
@@ -1,12 +1,11 @@
 import { describe, expect, it } from "vitest";
 
 import { WebCalendarClient } from "@/lib/web-calendar-client";
-import dotenv from "dotenv"
-import fs from "fs"
+import dotenv from "dotenv";
+import fs from "fs";
 import { twoGetEvents } from "testData/twoGetEvents";
 
-dotenv.config()
-
+dotenv.config();
 
 describe("WebCalendarClient.parseEvents", () => {
 	it("should not throw and have truthy value", () => {
@@ -15,12 +14,13 @@ describe("WebCalendarClient.parseEvents", () => {
 		expect(res).toBeTruthy();
 	});
 
-	it("should generate data that matches the dateEvents or dateTimeEvents schema", async() => {
-    const r:any = await fs.promises.readFile(process.env.TEST_GOOGLE_DATA_PATH as string)
-      .then((dat) => JSON.parse(dat.toString()))
-    
-    const largeDataSet:any[] = r.items
-    
+	it("should generate data that matches the dateEvents or dateTimeEvents schema", async () => {
+		const r: any = await fs.promises
+			.readFile(process.env.TEST_GOOGLE_DATA_PATH as string)
+			.then((dat) => JSON.parse(dat.toString()));
+
+		const largeDataSet: any[] = r.items;
+
 		const eventResults = WebCalendarClient.parseEvents(largeDataSet);
 		for (const event of eventResults.dateEvents) {
 			expect(event).toMatchObject({
@@ -34,24 +34,24 @@ describe("WebCalendarClient.parseEvents", () => {
 					date: expect.any(String),
 				},
 				dateType: "date",
-        changeType: expect.stringMatching("none")
+				changeType: expect.stringMatching("none"),
 			});
 		}
-    
-    eventResults.dateTimeEvents.forEach((event) => {
-      expect(event).toMatchObject({
-        id: expect.any(String),
-        summary: expect.any(String),
-        description: expect.any(String),
-        start: {
-          dateTime: expect.any(String),
-        },
-        end: {
-          dateTime: expect.any(String),
-        },
-        dateType: "dateTime",
-        changeType: expect.stringMatching("none")
-      });
-    })
+
+		eventResults.dateTimeEvents.forEach((event) => {
+			expect(event).toMatchObject({
+				id: expect.any(String),
+				summary: expect.any(String),
+				description: expect.any(String),
+				start: {
+					dateTime: expect.any(String),
+				},
+				end: {
+					dateTime: expect.any(String),
+				},
+				dateType: "dateTime",
+				changeType: expect.stringMatching("none"),
+			});
+		});
 	});
 });
